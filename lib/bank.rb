60560a47e111a9e6ed737b1e982a795253e1083e
JudeA94 <ashworthjude@gmail.com>
2023-01-23 14:06:59 +0000

    create and remove account methods test driven

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,18 @@
 class Bank
   def initialize(sort_code)
     @sort_code = sort_code
+    @accounts = []
   end
 
   attr_reader :sort_code
+  attr_accessor :accounts
+
+  def create_account(name)
+    @accounts.push({ name: name, sort_code: @sort_code, account_number: 12_345_678 })
+  end
+
+  def remove_account(account_number)
+    raise "That account doesn't exist" if @accounts.none? { |account| account[:account_number] === account_number }
+    @accounts.delete_if { |account| account[:account_number] === account_number }
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,5 +6,43 @@ describe Bank do
       bank = Bank.new('11-22-33')
       expect(bank.sort_code).to eq '11-22-33'
     end
+    it 'initializes with an empty array of accounts' do
+      bank = Bank.new('11-22-33')
+      expect(bank.accounts).to eq []
+    end
   end
+
+  context 'open_account method' do
+    it 'adds an account to the accounts list' do
+      bank = Bank.new('11-22-33')
+      bank.create_account('Test Name')
+      expect(bank.accounts.length).to eq 1
+    end
+    it 'the account has an account number and owner name' do
+      bank = Bank.new('11-22-33')
+      bank.create_account('Test Name')
+      expect(bank.accounts[0][:name]).to eq 'Test Name'
+      expect(bank.accounts[0][:account_number]).to be_a(Integer)
+    end
+  end
+
+  context 'delete_account method' do
+    it 'removes an account to the accounts list' do
+      bank = Bank.new('11-22-33')
+      bank.create_account('Test Name')
+      expect(bank.accounts.length).to eq 1
+      acc_number = bank.accounts[0][:account_number]
+      bank.remove_account(acc_number)
+      expect(bank.accounts.length).to eq 0
+    end
+    it "throws an error if the account doesn't exist" do
+      bank = Bank.new('11-22-33')
+      bank.create_account('Test Name')
+      expect(bank.accounts.length).to eq 1
+      acc_number = bank.accounts[0][:account_number]
+      expect { bank.remove_account(acc_number + 1) }.to raise_error "That account doesn't exist"
+      expect(bank.accounts.length).to eq 1
+    end
+  end
+
 end
